674a26a5155868a39773794734076bd1ea661518
tutran-se <trandinhtu.dev@gmail.com>
2024-03-09 21:53:37 -0500

    add 10k

--- src/main.rs
+++ src/main.rs
@@ -219,9 +219,7 @@ fn main() {
     // println!("Normal {:?}", duration);
 
     let starter = Instant::now();
-    for _ in 0..10000 {
-        sum_by_group_in_parallel(&tuple, 1707152400, 86400);
-    }
+    sum_by_group_in_parallel(&tuple, 1707152400, 86400);
     let duration = starter.elapsed();
     println!("Parallel: {:?}", duration);
 }
